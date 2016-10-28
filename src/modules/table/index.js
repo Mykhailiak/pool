df0c5270248d4b0adf6fb770906c387f9bf5dfd7
Yura Myhaylyak <ghost86421@gmail.com>
2016-10-29 00:15:54 +0300

    Init based containers

--- src/modules/table/index.js
+++ src/modules/table/index.js
@@ -1,20 +1,37 @@
 import * as PIXI from 'pixi.js';
 
-var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {backgroundColor: 0x1099bb}),
+var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {backgroundColor: 0x046131}),
 		stage = new PIXI.Container();
 
 document.body.appendChild(renderer.view);
 
 PIXI.loader
-	.add('resources/table.png')
+	.add('resources/bas_textures.json')
+	.add('resources/balls.json')
 	.load((e) => {
-		let table = new PIXI.Sprite(PIXI.loader.resources['resources/table.png'].texture);
+		let id = PIXI.loader.resources['resources/bas_textures.json'].textures;
 
-		table.scale.set(0.61, 0.61);
+		// game scene containers:
+		let gameScene = new PIXI.Container(),
+				gameOverScene = new PIXI.Container(),
+				gameIntroScene = new PIXI.Container(),
+				poolScene = new PIXI.Container();
 
-		table.position.set(renderer.view.width / 2 - table.width / 2, renderer.view.height / 2 - table.height / 2);
+		let table = new PIXI.Sprite(id['8ballpoolset-transparent.png']);
 
-		stage.addChild(table);
+
+		poolScene.addChild(table);
+		poolScene.position.set(renderer.width / 2 - poolScene.width / 2, renderer.height / 2 - poolScene.height / 2);
+
+		poolScene.addChild(table);
+
+		stage.addChild(gameScene);
+		stage.addChild(gameOverScene);
+		stage.addChild(gameIntroScene);
+		stage.addChild(poolScene);
+
+
+
 
 		renderer.render(stage);
 	});
