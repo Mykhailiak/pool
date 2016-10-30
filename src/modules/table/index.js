4bec2aaff63d814274a1385edec7b0f20d5310f9
Yura Myhaylyak <ghost86421@gmail.com>
2016-10-30 18:23:11 +0200

    add all balls into pool table

--- src/modules/table/index.js
+++ src/modules/table/index.js
@@ -4,6 +4,8 @@ import * as PIXI from 'pixi.js';
 var renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {backgroundColor: 0x046131}),
 		stage = new PIXI.Container();
 
+renderer.autoResize = true;
+
 let bump = new Bump.default(PIXI);
 
 document.body.appendChild(renderer.view);
@@ -13,7 +15,8 @@ PIXI.loader
 	.add('resources/balls.json')
 	.load((e) => {
 		let basic = PIXI.loader.resources['resources/bas_textures.json'].textures,
-				balls = PIXI.loader.resources['resources/balls.json'].textures;
+				balls = PIXI.loader.resources['resources/balls.json'].textures,
+				ballsArray = [];
 
 		// game scene containers:
 		let gameScene = new PIXI.Container(),
@@ -21,61 +24,34 @@ PIXI.loader
 				gameIntroScene = new PIXI.Container(),
 				poolScene = new PIXI.Container();
 
-		let table = new PIXI.Sprite(basic['8ballpoolset-transparent.png']),
-				ball1 = new PIXI.Sprite(balls['ball1.png']),
-				ball2 = new PIXI.Sprite(balls['ball2.png']);
+		let table = new PIXI.Sprite(basic['8ballpoolset-transparent.png']);
 
-		ball1.vx = 0;
-		ball1.vy = 0;
+		let xOffset = 55,
+				yOffset = 55;
 
 		poolScene.addChild(table);
-		poolScene.addChild(ball1);
-		poolScene.addChild(ball2);
-
 		poolScene.position.set(renderer.width / 2 - poolScene.width / 2, renderer.height / 2 - poolScene.height / 2);
-		ball1.position.set(200, poolScene.height / 2 - ball1.height / 2);
-		ball2.position.set(1000, (poolScene.height / 2 - ball2.height / 2));
 
-		ball1.circular = true;
-		ball2.circular = true;
+		for(let i = 1; i <= 15; i++) {
+			var ball = new PIXI.Sprite(balls[`ball${i}.png`]);
 
-		poolScene.hitArea = new PIXI.Rectangle(51, 51, 1022, 523);
+			ball.position.set(i * 32 + xOffset, i * 32 + yOffset);
+			ballsArray.push(ball);
+
+			ball.vx = 0;
+			ball.vy = 0;
+
+			poolScene.addChild(ball);
+		}
+
+
 
 		stage.addChild(gameScene);
 		stage.addChild(gameOverScene);
 		stage.addChild(gameIntroScene);
 		stage.addChild(poolScene);
 
-
-		console.log('bump', bump);
-		console.log('Pool', poolScene);
-
-		window.poolScene = poolScene;
-
-		ball1.vx = 10;
-		ball1.vy = 10;
-		ball2.vx = 3;
 		function animation() {
-
-			ball1.y += ball1.vy;
-			ball1.x += ball1.vx;
-
-			ball2.x += ball2.vx;
-
-			bump.contain(ball1, {x: 51, y: 51, width: 1082, height: 592}, true, (val) => {
-				console.log('Collapse', val);
-			});
-
-
-			if(bump.hit(ball1, ball2, true, true)) {
-				console.log('collapse', ball1.vx);
-			}
-
-			// console.log();
-
-			// if(bump.hitTestRectangle(ball1, poolScene.hitArea)) {
-			// 	console.log('boo');
-			// }
 
 			requestAnimationFrame(animation);
 
