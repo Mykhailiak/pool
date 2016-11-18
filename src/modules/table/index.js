468e1322856e79f14c03e3118a93d0642f1b21e5
Yura Myhaylyak <ghost86421@gmail.com>
2016-11-18 23:18:14 +0200

    #add board

--- src/modules/table/index.js
+++ src/modules/table/index.js
@@ -87,27 +87,43 @@ import * as PIXI from 'pixi.js';
 
 
 
-var renderer, stage, container, graphics, zoom,
+var renderer, stage, container, graphics, zoom, platform, platform2, platform3, platform4, platform5, platform6,
      world, boxShape, boxBody, planeBody, planeShape;
      init();
      animate();
      function init(){
          // Init p2.js
-         world = new p2.World({
-         	gravity:[0,-9.82]
-         });
+         world = new p2.World();
          // Add a box
          boxShape = new p2.Box({ width: 2, height: 1 });
          boxBody = new p2.Body({
-             mass:1,
-             position:[0,2],
+             mass: 1,
+             position:[0, 0],
              angularVelocity:1
          });
+
+         // Turn off global gravity
+         world.applyGravity=false;
+
+         // Keep track of which bodies you want to apply gravity on:
+         var gravityBodies=[boxBody];
+
+         // And just before running world.step(), do this:
+         var gravity = p2.vec2.fromValues(0, -50),
+             gravityForce = p2.vec2.create();
+         for(var i=0; i<gravityBodies.length; i++){
+             var b =  gravityBodies[i];
+             p2.vec2.scale(gravityForce,gravity,b.mass); // F_gravity = m*g
+             p2.vec2.add(b.force,b.force,gravityForce);  // F_body += F_gravity
+         }
+
+         debugger;
+
          boxBody.addShape(boxShape);
          world.addBody(boxBody);
          // Add a plane
          planeShape = new p2.Plane();
-         planeBody = new p2.Body({ position:[0,-3] });
+         planeBody = new p2.Body({ position:[0, 0] });
          planeBody.addShape(planeShape);
          world.addBody(planeBody);
          // Pixi.js zoom level
@@ -115,6 +131,88 @@ var renderer, stage, container, graphics, zoom,
          // Initialize the stage
          renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight, {backgroundColor: 0x046131}),
          stage = new PIXI.Stage(0xFFFFFF);
+
+
+
+         
+         // Create static platform box
+         var platformBody1 = new p2.Body({
+                 mass: 0, // static
+                 position: [0.36, -3]
+             }),
+             platformShape1 = new p2.Box({
+                 width: 4.32,
+                 height: 0.45,
+                 material: new p2.Material()
+             });
+         platformBody1.addShape(platformShape1);
+         world.addBody(platformBody1);
+
+         // Create static platform box
+         var platformBody2 = new p2.Body({
+                 mass: 0, // static
+                 position: [-4.68, -3]
+             }),
+             platformShape2 = new p2.Box({
+                 width: 4.32,
+                 height: 0.45,
+                 material: new p2.Material()
+             });
+         platformBody2.addShape(platformShape2);
+         world.addBody(platformBody2);
+
+         // Create static platform box
+         var platformBody3 = new p2.Body({
+                 mass: 0, // static
+                 position: [0.36, 2.76]
+             }),
+             platformShape3 = new p2.Box({
+                 width: 4.31,
+                 height: 0.45,
+                 material: new p2.Material()
+             });
+         platformBody3.addShape(platformShape3);
+         world.addBody(platformBody3);
+
+         // Create static platform box
+         var platformBody4 = new p2.Body({
+                 mass: 0, // static
+                 position: [-4.64, 2.75]
+             }),
+             platformShape4 = new p2.Box({
+                 width: 4.3,
+                 height: 0.46,
+                 material: new p2.Material()
+             });
+         platformBody4.addShape(platformShape4);
+         world.addBody(platformBody4);
+
+         // Create static platform box
+         var platformBody5 = new p2.Body({
+                 mass: 0, // static
+                 position: [-5.59, -2.12]
+             }),
+             platformShape5 = new p2.Box({
+                 width: 0.46,
+                 height: 4.439,
+                 material: new p2.Material()
+             });
+         platformBody5.addShape(platformShape5);
+         world.addBody(platformBody5);
+
+         // Create static platform box
+         var platformBody6 = new p2.Body({
+                 mass: 0, // static
+                 position: [5.14, -2.12]
+             }),
+             platformShape6 = new p2.Box({
+                 width: 0.46,
+                 height: 4.439,
+                 material: new p2.Material()
+             });
+         platformBody6.addShape(platformShape6);
+         world.addBody(platformBody6);
+
 
 
          PIXI.loader
@@ -153,9 +251,63 @@ var renderer, stage, container, graphics, zoom,
          				graphics = new PIXI.Graphics();
          				graphics.beginFill(0xff0000);
          				graphics.drawRect(-boxShape.width/2, -boxShape.height/2, boxShape.width, boxShape.height);
-         				// Add the box to our container
          				container.addChild(graphics);
-         	})
+
+
+
+         				platform = new PIXI.Graphics();
+         				platform.beginFill(0xff0000);
+         				platform.drawRect(platformBody1.position[0], platformBody1.position[1] - 0.1, platformShape1.width, platformShape1.height);
+         				// Add the box to our container
+         				container.addChild(platform);
+
+         				platform2 = new PIXI.Graphics();
+         				platform2.beginFill(0xff0000);
+         				platform2.drawRect(platformBody2.position[0], platformBody2.position[1] - 0.1, platformShape2.width, platformShape2.height);
+         				// Add the box to our container
+         				container.addChild(platform2);
+
+         				platform3 = new PIXI.Graphics();
+         				platform3.beginFill(0xff0000);
+         				platform3.drawRect(platformBody3.position[0], platformBody3.position[1] - 0.1, platformShape3.width, platformShape3.height);
+         				// Add the box to our container
+         				container.addChild(platform3);
+
+         				platform4 = new PIXI.Graphics();
+         				platform4.beginFill(0xff0000);
+         				platform4.drawRect(platformBody4.position[0], platformBody4.position[1] - 0.1, platformShape4.width, platformShape4.height);
+         				// Add the box to our container
+         				container.addChild(platform4);
+
+         				platform5 = new PIXI.Graphics();
+         				platform5.beginFill(0xff0000);
+         				platform5.drawRect(platformBody5.position[0], platformBody5.position[1] - 0.1, platformShape5.width, platformShape5.height);
+         				// Add the box to our container
+         				container.addChild(platform5);
+
+         				platform6 = new PIXI.Graphics();
+         				platform6.beginFill(0xff0000);
+         				platform6.drawRect(platformBody6.position[0], platformBody6.position[1] - 0.1, platformShape6.width, platformShape6.height);
+         				// Add the box to our container
+         				container.addChild(platform6);
+         	});
+
+
+
+         	// Turn off global gravity
+         	// world.applyGravity=false;
+
+         	// // Keep track of which bodies you want to apply gravity on:
+         	// var gravityBodies=[boxBody];
+
+         	// // And just before running world.step(), do this:
+         	// var gravity = p2.vec2.fromValues(0,-9.82),
+         	//     gravityForce = p2.vec2.create();
+         	// for(var i=0; i<gravityBodies.length; i++){
+         	//     var b =  gravityBodies[i];
+         	//     p2.vec2.scale(gravityForce,gravity,b.mass); // F_gravity = m*g
+         	//     p2.vec2.add(b.force,b.force,gravityForce);  // F_body += F_gravity
+         	// }
 
 
      }
